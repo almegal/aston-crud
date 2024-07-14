0b841c55cedd53999f884215a42a6d62605e5d26
alekseipeikhel <alekseipeikhel@gmail.com>
2024-07-14 06:42:15 +0300

    выносс логики респонса из утилиты в сервлет

--- src/main/java/servlet/util/ApiUtils.java
+++ src/main/java/servlet/util/ApiUtils.java
@@ -1,34 +1,31 @@
 package servlet.util;
 
-import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
 import dto.ProductDTO;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class ApiUtils {
-    static private final Gson gson = new GsonBuilder().serializeNulls().create();
 
-    static public ProductDTO convertJsonToProductDTO(HttpServletRequest request) throws IOException {
-        Scanner scanner = new Scanner(request.getInputStream(), StandardCharsets.UTF_8);
-        String jsonData = scanner.useDelimiter("\\A").next();
-        scanner.close();
-        return gson.fromJson(jsonData, ProductDTO.class);
+    static public ProductDTO convertJsonToProductDTO(HttpServletRequest request) throws ServletException {
+        try {
+            Scanner scanner = new Scanner(request.getInputStream(), StandardCharsets.UTF_8);
+            String jsonData = scanner.useDelimiter("\\A").next();
+            scanner.close();
+            return new GsonBuilder()
+                    .serializeNulls()
+                    .create()
+                    .fromJson(jsonData, ProductDTO.class);
+        } catch (JsonSyntaxException | IOException e) {
+            throw new ServletException(e);
+        }
     }
 
-    static public void sendJsonResponse(HttpServletResponse response, ProductDTO productDTO) throws IOException {
-        response.setStatus(HttpServletResponse.SC_OK);
-        response.setContentType("application/json");
-        PrintWriter writer = response.getWriter();
-        writer.print(gson.toJson(productDTO));
-        writer.flush();
-    }
 
     static public String splitPathInfo(HttpServletRequest request) throws ServletException {
         if (request.getPathInfo() == null) {
